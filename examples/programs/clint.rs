d5a22beba2b400f9051a2f3630ed9a966f2dcdc4
石博文 <sbw@sbw.so>
2017-11-26 17:27:47 +0800

    bump id_tree version

---
+++ examples/programs/clint.rs
@@ -0,0 +1,16 @@
+
+extern crate parser;
+
+use parser::lexer::*;
+use parser::parser::*;
+use parser::parser::recursive_descent::*;
+
+fn main() {
+    let src = "a + b != c + d || !e";
+    let mut parser = RecursiveDescentParser::new(Lexer::new(src.as_bytes()));
+
+    println!("\n{}\n", src);
+
+    parser.run();
+    parser.dump();
+}
