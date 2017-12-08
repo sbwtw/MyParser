d7074f8318c66c06819ef6af499d15109de3a646
石博文 <sbw@sbw.so>
2017-12-08 10:16:41 +0800

    add build depends in travis CI

--- examples/programs/llvm.rs
+++ examples/programs/llvm.rs
@@ -9,7 +9,7 @@ fn main() {
         let builder = LLVMCreateBuilderInContext(context);
 
         let i64t = LLVMInt64TypeInContext(context);
-        let mut argts = [i64t, i64t];
+        let mut argts = [i64t, i64t, i64t];
         let function_type = LLVMFunctionType(i64t, argts.as_mut_ptr(), argts.len() as u32, 0);
 
         let function = LLVMAddFunction(module, b"sum\0".as_ptr() as *const _, function_type);
@@ -17,9 +17,19 @@ fn main() {
         let bb = LLVMAppendBasicBlockInContext(context, function, b"entry\0".as_ptr() as *const _);
 
         LLVMPositionBuilderAtEnd(builder, bb);
+        let x = LLVMGetParam(function, 0);
+        let y = LLVMGetParam(function, 1);
+        let z = LLVMGetParam(function, 2);
 
-        // LLVMBuildRet(builder, sum);
+        let sum = LLVMBuildAdd(builder, x, y, b"sum.1\0".as_ptr() as *const _);
+        let sum = LLVMBuildAdd(builder, sum, z, b"sum.2\0".as_ptr() as *const _);
+
+        LLVMBuildRet(builder, sum);
+
         LLVMDisposeBuilder(builder);
         LLVMDumpModule(module);
+
+        // clean
+        LLVMContextDispose(context);
     }
 }
